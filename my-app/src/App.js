cd1c720565efa8b9cad8b5a3fc15ed50b56f1cf6
Roux Long <rclong1221@gmail.com>
2022-07-31 13:56:43 -0700

    initial react app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+    return (
+        <div>
+            <h2>H2</h2>
+            <ExpenseItem></ExpenseItem>
+        </div>
+    )
+}
+
+export default App;
---
+++ my-app/src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>
+                <div>12/21/1985</div>
+            </div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.67</div>
+            </div>
+        </div>
+        
+    );
+}
+
+export default ExpenseItem;
