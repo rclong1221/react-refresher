9bd5b7fb2dffe5af8330b710f81d7f3bff3c67dc
Roux Long <rclong1221@gmail.com>
2022-08-01 13:00:18 -0700

    hook up chart

--- my-app/src/components/Expenses/ExpensesChart.js
+++ my-app/src/components/Expenses/ExpensesChart.js
@@ -19,7 +19,6 @@ function ExpensesChart(props) {
     for (const expense of props.expenses) {
         const expenseMonth = expense.date.getMonth();
         chartDataPoints[expenseMonth].value += expense.amount;
-        console.log(expense);
     }
 
     return (
