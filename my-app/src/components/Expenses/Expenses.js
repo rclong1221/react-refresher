c09548ab9da9e6329babc6cac4c27eec8ef26c64
Roux Long <rclong1221@gmail.com>
2022-08-01 01:37:44 -0700

    add year filter

--- my-app/src/components/Expenses/Expenses.js
+++ my-app/src/components/Expenses/Expenses.js
@@ -1,12 +1,26 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
+    const [year, setYear] = useState('2022');
+
+    function onFilterHandler(yr) {
+        setYear(yr);
+    }
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === year;
+    });
+
     return (
         <Card className="expenses">
-            {props.items.map((expense) => { return (
+            <ExpensesFilter onFilterHandler={onFilterHandler}></ExpensesFilter>
+            {filteredExpenses.map((expense) => { return (
                 <ExpenseItem 
                 key={expense.id}
                 date={expense.date}
