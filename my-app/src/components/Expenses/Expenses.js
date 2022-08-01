43e2b3876d85a4aa7bb6ab61bdbad6d66b5394c4
Roux Long <rclong1221@gmail.com>
2022-08-01 10:16:50 -0700

    add conditional render component and styles

--- my-app/src/components/Expenses/Expenses.js
+++ my-app/src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
+
 import Card from '../UI/Card';
 
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
     const [year, setYear] = useState('2022');
@@ -17,21 +18,10 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === year;
     });
 
-    const mappedExpenses = filteredExpenses.map((expense) => { return (
-        <ExpenseItem 
-        key={expense.id}
-        date={expense.date}
-        title={expense.title}
-        amount={expense.amount}
-    ></ExpenseItem>
-    )});
-
-    const noExpenses = <p>No expenses found.</p>;
-
     return (
         <Card className="expenses">
             <ExpensesFilter onFilterHandler={onFilterHandler}></ExpensesFilter>
-            {filteredExpenses.length === 0 ? noExpenses : mappedExpenses}
+            <ExpensesList items={filteredExpenses}></ExpensesList>
         </Card>
     )
 }
