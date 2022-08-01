60ae4304369e90dd1f0e5597ea03f13b1d3e548b
Roux Long <rclong1221@gmail.com>
2022-07-31 20:29:59 -0700

    add newexpense component

---
+++ my-app/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NewExpense(props) {
+    return (
+        <div className="new-expense">
+            <form></form>
+        </div>
+    )
+}
+
+export default NewExpense;
