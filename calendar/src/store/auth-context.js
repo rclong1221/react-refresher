2f83739cbc4de5ca4382f034f9d387dd2b952a5d
Roux Long <rclong1221@gmail.com>
2022-08-05 00:33:39 -0700

    add authcontext provider and export

--- calendar/src/store/auth-context.js
+++ calendar/src/store/auth-context.js
@@ -6,3 +6,32 @@ const AuthContext = React.createContext({
     login: (token) => {},
     logout: () => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [token, setToken] = useState(null);
+
+    const userIsLoggedIn = !!token;
+
+    const loginHandler = (token) => {
+        setToken(token);
+    };
+
+    const logoutHandler = () => {
+        setToken(null);
+    };
+
+    const contextValue = {
+        token: token,
+        isLoggedIn: userIsLoggedIn,
+        login: loginHandler,
+        logout: logoutHandler
+    };
+
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthContext;
