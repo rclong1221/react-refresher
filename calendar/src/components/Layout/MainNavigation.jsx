3b1b006275ecdcea3a3f65b582cbf9067884da8f
Roux Long <rclong1221@gmail.com>
2022-08-09 11:31:03 -0700

    add calendar to nav

--- calendar/src/components/Layout/MainNavigation.jsx
+++ calendar/src/components/Layout/MainNavigation.jsx
@@ -17,6 +17,9 @@ const MainNavigation = () => {
             <Link to='/profile'>Profile</Link>
           </li>
           <li>
+            <Link to='/calendar'>Calendar</Link>
+          </li>
+          <li>
             <button>Logout</button>
           </li>
         </ul>
