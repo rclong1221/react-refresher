45e4158cfe0f534f23979cc531366dfa71240ab4
Roux Long <rclong1221@gmail.com>
2022-08-24 22:37:14 -0700

    add patch profile request

--- calendar/src/components/Profile/ProfileForm.jsx
+++ calendar/src/components/Profile/ProfileForm.jsx
@@ -1,26 +1,108 @@
+import { useState } from 'react';
+import { useContext } from 'react';
+import { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory();
+  const usernameInputRef = useRef();
+  const nameInputRef = useRef();
+  const firstNameInputRef = useRef();
+  const lastNameInputRef = useRef();
+  const middleNameInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const switchAuthModeHandler = () => {
+    props.switchAuthModeHandler();
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredUsername = usernameInputRef.current.value;
+    const enteredName = nameInputRef.current.value;
+    const enteredFirstName = firstNameInputRef.current.value;
+    const enteredLastName = lastNameInputRef.current.value;
+    const enteredMiddleName = middleNameInputRef.current.value;
+
+    // optional: Add validation
+    setIsLoading(true);
+
+
+    let url = 'http://127.0.0.1:8000/api/users/803981b7-a755-4719-9321-9190d8878ed5/';
+    let data = {
+        "username": enteredUsername,
+        "name": enteredName,
+        "first_name": enteredFirstName,
+        "last_name": enteredLastName,
+        "middle_name": enteredMiddleName
+    }
+
+    console.log(data);
+    
+
+    fetch(url, {
+      method: 'PATCH',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 54438e4191f37f626c664048407577962b08c393'
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          alert(res);
+          return res.json();
+        } else {
+          return res.json().then((resp) => {
+            let errorMessage = 'Authentication failed!';
+            // if (resp && resp.error && resp.error.message) {
+            //   errorMessage = resp.error.message;
+            // }
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((d) => {
+        alert(d);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+
   return (
     <form className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='username'>Username</label>
-        <input type='text' id='username' />
+        <input type='text' id='username' ref={usernameInputRef}/>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='name'>Name</label>
+        <input type='text' id='name' ref={nameInputRef}/>
       </div>
       <div className={classes.control}>
         <label htmlFor='first-name'>First</label>
-        <input type='text' id='first-name' />
+        <input type='text' id='first-name' ref={firstNameInputRef}/>
       </div>
       <div className={classes.control}>
         <label htmlFor='middle-name'>Middle</label>
-        <input type='text' id='middle-name' />
+        <input type='text' id='middle-name' ref={middleNameInputRef}/>
       </div>
       <div className={classes.control}>
         <label htmlFor='last-name'>Last</label>
-        <input type='text' id='last-name' />
+        <input type='text' id='last-name' ref={lastNameInputRef}/>
       </div>
       <div className={classes.action}>
-        <button>Update Profile</button>
+        <button onClick={submitHandler}>Update Profile</button>
       </div>
     </form>
   );
