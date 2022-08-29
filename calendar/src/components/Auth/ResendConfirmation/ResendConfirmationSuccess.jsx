546bee53c68c9bbaabb4124ef2009109abca3afd
Roux Long <rclong1221@gmail.com>
2022-08-29 16:19:08 -0700

    add resendconfirmationsuccess message

---
+++ calendar/src/components/Auth/ResendConfirmation/ResendConfirmationSuccess.jsx
@@ -0,0 +1,9 @@
+const ResendConfirmationSuccess = () => {
+  return (
+    <>
+      <h2>If an account is registered to your email, a confirmation will be sent.</h2>
+    </>
+  );
+}
+
+export default ResendConfirmationSuccess;
