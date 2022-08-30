29814b377882af4f41f037473891b6e852db0dbe
Roux Long <rclong1221@gmail.com>
2022-08-29 23:53:44 -0700

    redirect if already logged in

--- calendar/src/components/Auth/AuthForm.jsx
+++ calendar/src/components/Auth/AuthForm.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import AuthContext from '../../store/auth-context';
 
 import classes from './AuthForm.module.css';
 
@@ -6,19 +9,25 @@ import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
 
 const AuthForm = () => {
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const history = useHistory();
+
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const form = () => {
+    if (isLogin) return <><LoginForm switchAuthModeHandler={switchAuthModeHandler} /></>
+    return <><RegisterForm switchAuthModeHandler={switchAuthModeHandler} /></> 
+  }
+
   return (
     <section className={classes.auth}>
-      { 
-        isLogin 
-        ? <LoginForm switchAuthModeHandler={switchAuthModeHandler} /> 
-        : <RegisterForm switchAuthModeHandler={switchAuthModeHandler} /> 
-      }
+      { isLoggedIn ? history.replace('/calendar/') : form() }
     </section>
   );
 };
