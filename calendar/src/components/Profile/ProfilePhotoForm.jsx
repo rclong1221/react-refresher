eddc9622f1dead881d9304766b75b0e84b1a1ddd
Roux Long <rclong1221@gmail.com>
2022-09-01 14:05:15 -0700

    add update profile toasts

--- calendar/src/components/Profile/ProfilePhotoForm.jsx
+++ calendar/src/components/Profile/ProfilePhotoForm.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { useContext } from 'react';
 import { useRef } from 'react';
 import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import AuthContext from '../../store/auth-context';
 import ProfilePhoto from './ProfilePhoto';
 import classes from './ProfilePhotoForm.module.css';
@@ -24,11 +27,22 @@ const ProfilePhotoForm = () => {
     // optional: Add validation
     setIsLoading(true);
 
+    const updateFailed = () => {
+      toast.error('Update failed!', {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
 
     let url = 'http://127.0.0.1:8000/api/user_image/';
     let data = new FormData();
 
-    if (enteredImage) {
+    if (!!enteredImage[0]) {
       data.append('image', enteredImage[0]);
       data.append('user', `${user.uuid}`);
 
@@ -43,22 +57,36 @@ const ProfilePhotoForm = () => {
         console.log(res);
         setIsLoading(false);
         if (res.ok) {
-          console.log("Picture has been updated!")
+          console.log("Picture has been updated!");
+          toast.success('🦄 Picture updated!', {
+            position: "bottom-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
         } else {
-          console.log("Update failed")
           return res.json().then((resp) => {
-            console.log(resp);
-            throw new Error(resp);
+            updateFailed();
           });
         }
       })
       .catch((err) => {
-        console.log(err);
-        alert(err.message);
+        updateFailed();
       });
     }
     else {
-      console.log("Select an image!");
+      toast.warning('Select an image.', {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
   };
 
